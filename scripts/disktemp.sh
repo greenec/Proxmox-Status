fe16a4c535769acad6df435ab28f4d4ac9592d44
Connor Greene <connor@connorgreene.net>
2021-12-16 13:22:42 -0500

    Print device model with SSD output

--- scripts/disktemp.sh
+++ scripts/disktemp.sh
@@ -10,20 +10,21 @@ else
 fi
 
 output=$(
-        for disk in ${disks[@]}; do
-                realpath=$(realpath "/dev/disk/by-id/$disk")
-                hddtemp=$(sudo hddtemp "$realpath" 2>&1)
+	for disk in ${disks[@]}; do
+		realpath=$(realpath "/dev/disk/by-id/$disk")
+		hddtemp=$(sudo hddtemp "$realpath" 2>&1)
 
-				# handle SSDs which use a different field for temperature
-				if [[ "$hddtemp" == *"doesn't seem to have a temperature sensor"* ]]; then
-					realpath=$(realpath "/dev/disk/by-id/$disk")
-                	ssdtemp=$(sudo hddtemp -D "$realpath" | awk '$0 ~ /field\(190\)/ { print $3 }')
+		# handle SSDs which use a different field for temperature
+		if [[ "$hddtemp" == *"doesn't seem to have a temperature sensor"* ]]; then
+			realpath=$(realpath "/dev/disk/by-id/$disk")
+			ssdtemp=$(sudo hddtemp -D "$realpath" | awk '$0 ~ /field\(190\)/ { print $3 }')
+			diskname=$(sudo smartctl -a "$realpath" | sed -n 's/Device Model:\s*\(.*\)/\1/p')
 
-                	printf "$realpath: $ssdtemp\u00b0C|[$disk]\n"
-				else
-                	printf "$hddtemp|[$disk]\n"
-				fi
-        done
+			printf "$realpath: $diskname: $ssdtemp\u00b0C|[$disk]\n"
+		else
+			printf "$hddtemp:|[$disk]\n"
+		fi
+	done
 )
 
 # sort output by disk letter, e.g. /dev/sda
