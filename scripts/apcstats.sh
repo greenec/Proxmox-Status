0ea2d0d9a1dbc16f03f049542dcb16c958839e65
Connor Greene <connor@connorgreene.net>
2021-12-19 21:54:55 -0500

    Add time remaining printout to APC UPS script

--- scripts/apcstats.sh
+++ scripts/apcstats.sh
@@ -10,8 +10,10 @@ ups_info=$(/sbin/apcaccess)
 nominal_power=$(awk '{ if ($1 == "NOMPOWER") { print $3 } }' <<< "$ups_info")
 power_unit=$(awk '{ if ($1 == "NOMPOWER") { print $4 } }' <<< "$ups_info")
 load_percent=$(awk '{ if ($1 == "LOADPCT") { print $3 } }' <<< "$ups_info")
+time_left=$(awk '{ if ($1 == "TIMELEFT") { print $3 " " $4 } }' <<< "$ups_info")
 
 power=$(bc <<< "scale=2; $nominal_power * $load_percent / 100.0")
 
 echo "APC UPS Draw: $power $power_unit ($load_percent %)"
+echo "Time Remaining: $time_left"
 
--- scripts/cpuinfo.sh
+++ scripts/cpuinfo.sh
@@ -3,13 +3,11 @@
 dir=$(dirname $(realpath "$0"))
 source $dir/../config.sh
 
-
+# print load average info
 awk '{ print( "Load average:\t" $1 " (1m)\t" $2 " (5m)\t" $3 " (15m)" ) }' < /proc/loadavg
 
 
-echo
-
-
+# print cpu temperature
 temp=$(sensors $cpu_temp_device | awk "\$0 ~ /$cpu_temp_field_label/ { print $cpu_temp_awk_print_fmt }")
 printf "%s:\t%s\n" "$cpu_temp_device" "$temp"
 
