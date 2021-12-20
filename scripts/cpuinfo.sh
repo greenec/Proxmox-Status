df017df0049dfc88f307c9eff7be27ee2c7da300
Connor Greene <connor@connorgreene.net>
2021-12-19 22:07:46 -0500

    Format cpuinfo and apcstats output with column

--- scripts/cpuinfo.sh
+++ scripts/cpuinfo.sh
@@ -3,11 +3,13 @@
 dir=$(dirname $(realpath "$0"))
 source $dir/../config.sh
 
-# print load average info
-awk '{ print( "Load average:\t" $1 " (1m)\t" $2 " (5m)\t" $3 " (15m)" ) }' < /proc/loadavg
-
-
 # print cpu temperature
 temp=$(sensors $cpu_temp_device | awk "\$0 ~ /$cpu_temp_field_label/ { print $cpu_temp_awk_print_fmt }")
-printf "%s:\t%s\n" "$cpu_temp_device" "$temp"
+
+column -t -s '|' <<< $(
+	# print load average info
+	awk '{ print( "Load average:|" $1 " (1m)\t" $2 " (5m)\t" $3 " (15m)" ) }' < /proc/loadavg
+
+	printf "%s:|%s\n" "$cpu_temp_device" "$temp"
+)
 
