53b89d884fd728bd51af4a92c911e7e40c7258f3
Connor Greene <connor@connorgreene.net>
2022-07-31 21:21:32 -0400

    Added hard drive SMART stats script

---
+++ scripts/smartstats.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CYAN='\033[0;36m'
+NC='\033[0m'
+
+dir=$(dirname "$(realpath "$0")")
+source "$dir/../config.sh"
+
+header_printed=false
+
+for disk in "${harddisks[@]}"; do
+	smartctl=$(sudo smartctl -a "/dev/disk/by-id/$disk")
+
+	if [ "$header_printed" = false ]; then
+		echo "$smartctl" | grep 'ID#'
+		header_printed=true
+	fi
+
+	printf "${CYAN}${disk}${NC}\n"
+	echo "$smartctl" | grep 'Reallocated_Sector_Ct'
+	echo "$smartctl" | grep 'Current_Pending_Sector'
+	printf "\n"
+done
+
