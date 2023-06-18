72092d457aecce2a199720fb77eae2348061b059
Connor Greene <connor@connorgreene.net>
2023-06-17 23:07:24 -0400

    Reduce 'sudo' usage in disktemp script

--- scripts/disktemp.sh
+++ scripts/disktemp.sh
@@ -12,13 +12,23 @@ fi
 output=$(
 	for disk in "${disks[@]}"; do
 		realpath=$(realpath "/dev/disk/by-id/$disk")
-		hddtemp=$(sudo hddtemp "$realpath" 2>&1)
+		hddtemp_cmd="/sbin/hddtemp $realpath 2>&1"
+		if [ "$EUID" -ne 0 ]; then
+			hddtemp_cmd="sudo $hddtemp_cmd"
+		fi
+		hddtemp=$(eval "$hddtemp_cmd")
 
 		# handle SSDs which use a different field for temperature
 		if [[ "$hddtemp" == *"doesn't seem to have a temperature sensor"* ]]; then
 			realpath=$(realpath "/dev/disk/by-id/$disk")
-			ssdtemp=$(sudo hddtemp -D "$realpath" | awk '/field\(190\)/ { print $3 }')
-			diskname=$(sudo smartctl -a "$realpath" | sed -n 's/Device Model:\s*\(.*\)/\1/p')
+			ssdtemp_cmd="/sbin/hddtemp -D $realpath | awk '/field\(190\)/ { print \$3 }'"
+			diskname_cmd="/sbin/smartctl -a $realpath | sed -n 's/Device Model:\s*\(.*\)/\1/p'"
+			if [ "$EUID" -ne 0 ]; then
+				ssdtemp_cmd="sudo $ssdtemp_cmd"
+				diskname_cmd="sudo $diskname_cmd"
+			fi
+			ssdtemp=$(eval "$ssdtemp_cmd")
+			diskname=$(eval "$diskname_cmd")
 
 			printf "%s:|%s:|%s\u00b0C:|[%s]\n" "$realpath" "$diskname" "$ssdtemp" "$disk"
 		else
